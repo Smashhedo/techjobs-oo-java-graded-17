7c620b0cfeb7ec1553cc777b624f8563668f8e75
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-10-03 23:28:37 -0500

    Task4 Needs Work

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -2,7 +2,7 @@ package org.launchcode.techjobs.oo;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.*;
 
 
 public class JobTest {
@@ -16,6 +16,35 @@ public class JobTest {
 
     @Test
     public void testJobConstructorSetsAllFields() {
-        new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        //below is the declared and initialized new Job object
+                Job aJobObject3 = new Job("Product tester", new Employer("ACME"),
+                                  new Location("Desert"), new PositionType("Quality control"),
+                                  new CoreCompetency("Persistence"));
+
+//                assertTrue();
+//                assertTrue();
+//                assertTrue();
+//                assertTrue();
+//                assertTrue();
+
+                assertEquals("Product tester", aJobObject3.getName());
+                assertEquals("ACME", aJobObject3.getEmployer());
+                assertEquals("Desert", aJobObject3.getLocation());
+                assertEquals("Quality control", aJobObject3.getPositionType());
+                assertEquals("Persistence", aJobObject3.getCoreCompetency());
+
         }
+
+    @Test
+    public void testJobsForEquality() {
+        Job aJobObject4 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+        Job aJobObject5 = new Job("Product tester", new Employer("ACME"),
+                new Location("Desert"), new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        aJobObject4.equals(aJobObject5);
+
+    }
 }
