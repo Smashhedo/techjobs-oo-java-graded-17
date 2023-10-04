8619d8a524e62c11013da9725ea5aaa5aab813a7
Danielle <136658130+Smashhedo@users.noreply.github.com>
2023-10-03 22:02:39 -0500

    Task4 Cat Stepped On Keyboard

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,21 @@
 package org.launchcode.techjobs.oo;
 
+import org.junit.Test;
+
+import static org.junit.Assert.assertNotEquals;
+
+
 public class JobTest {
     //TODO: Create your unit tests here
+    @Test
+    public void testSettingJobId() {
+            Job aJobObject1 = new Job();
+            Job aJobObject2 = new Job();
+            assertNotEquals(aJobObject1.getId(), aJobObject2.getId());
+        }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+        }
 }
